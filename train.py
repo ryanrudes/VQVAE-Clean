8fb0d6a2ebc408f0628b72efa8b42e3ac2140e1a
Ryan Rudes <ryanrudes@gmail.com>
2021-04-26 10:41:11 -0400

    Fixed progress bar

--- train.py
+++ train.py
@@ -1,25 +1,6 @@
 import distributed as dist
 from vqvae import VQVAE
-from torch import optim
-import sys
-import os
 
-class Args:
-    n_gpu = 1
-    port = 2 ** 15 + 2 ** 14 + hash(os.getuid() if sys.platform != "win32" else 1) % 2 ** 14
-    dist_url = f'tcp://127.0.0.1:{port}'
-    size = 256
-    epoch = 560
-    lr = 3e-4
-    batch_size = 128
-    num_workers = 4
-    normalize = True
-    optimizer = optim.Adam
-    distributed = dist.get_world_size() > 1
-    sched = ''
-    path = ''
-
-args = Args()
-model = VQVAE()
-
-dist.launch(model.run, args.n_gpu, 1, 0, args.dist_url, args = (args,))
+def train(args):
+    model = VQVAE()
+    dist.launch(model.run, args.n_gpu, 1, 0, args.dist_url, args = (args,))
